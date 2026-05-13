2f038bd31c802099c31921776ce601f6dc1adcec
Muhammad Mizan Al Mujadid <mizanmujadid2436@gmail.com>
2026-05-13 12:28:52 +0700

    feat(perf): configure next/image for R2 and add global skeleton styles

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['promodernistic-distensible-kieth.ngrok-free.dev'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-portofolio.mzn.my.id',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
